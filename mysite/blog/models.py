648f3828fbd18601fbd3acac042788f98565bf2d
Chiken-Kitchen <filkargin@yandex.ru>
2025-03-31 15:38:56 +0300

    Создал модель Поста и подмодель статуса с парметрами (черновик\опубликовано), добавил элементы индексации и упорядочивания, стандартный литерал Поста

---
+++ mysite/blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    """Модель поста с характеристиками:
+    Название, слаг поста, тело поста(текст)."""
+    class Status(models.TextChoices):
+        """Модель статуса, отражающая является ли пост
+        черновиком или опубликован."""
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=256,)
+    slug = models.SlugField(max_length=256,)
+    body = models.TextField()
+    pub_date = models.DateTimeField(default=timezone.now(),)
+    created = models.DateTimeField(auto_now_add=True,)
+    updated = models.DateTimeField(auto_now=True,)
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+        indexes = [
+            models.Index(fields=['-pub_date']),
+        ]
+
+    def __str__(self):
+        return self.title
