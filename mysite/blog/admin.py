3dcd4b2d5f9308c6cf852ff5a41013e815624afe
Chiken-Kitchen <filkargin@yandex.ru>
2025-03-31 16:24:23 +0300

    добавили в админку модель Поста, а так  же определили удобные элементы, такие как автозаполнение Слага поста, поиск автора (удобно когда авторов кратно больше сотни), строка поиска постов по телу и названию, а так же иерархия выдачи поисковых запросов по дате публикаций

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # Отображает поля модели на странице Администрирования
+    list_display = ['title', 'slug', 'author', 'pub_date', 'status']
+    list_filter = ['status', 'created', 'pub_date', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'pub_date'
+    ordering = ['status', 'pub_date']
