fd46c32c2afaa5f49aa97b6e33a7605f41d44631
Chiken-Kitchen <filkargin@yandex.ru>
2025-03-31 19:47:17 +0300

    Добавили примитивные функции отображения постов с писком и детального оторбражения поста, а так же добавили урлы для этих представлений

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+
+from .models import Post
+
+
+def post_list(request):
+    """Отображение информации о посте."""
+
+    posts = Post.published.all() # Получаем все опубликованные объекты.
+    context = {'posts': posts}
+    # Закидываем в отображение резульатата реквест, шаблон и контекст
+    return render(request, 'blog/post/list.html', context)
+
+def post_detail(request, id):
+    """Функция детального тображения одного опубликованного поста."""
+
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status=Post.Status.PUBLISHED
+    )
+    context = {'post': post}
+    return render(request, 'blog/post/detail.html', context)
